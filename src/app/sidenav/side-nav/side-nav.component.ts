1cb1340ba65cadf64d47a80416da01d7332b832d
stefanl <stefan.lezaic@plusplusnt.rs>
2023-02-16 00:29:06 +0100

    [develop] side nav

--- src/app/sidenav/side-nav/side-nav.component.ts
+++ src/app/sidenav/side-nav/side-nav.component.ts
@@ -7,6 +7,8 @@ import {select, Store} from "@ngrx/store";
 import {selectLoginUser} from "../../modules/user/store/selectors";
 import {UserActions} from "../../modules/user";
 import {Router} from "@angular/router";
+import {selectSelectedGroup} from "../../modules/group/store/selectors";
+import {Group} from "../../modules/group/model/group";
 
 @Component({
   selector: 'app-side-nav',
@@ -17,6 +19,7 @@ export class SideNavComponent implements OnInit {
 
   items: SideNavSection[] = [];
   user: User | undefined;
+  selectedGroup: Group | undefined;
   private ngUnsubscribe: Subject<void> = new Subject<void>();
 
 
@@ -39,38 +42,47 @@ export class SideNavComponent implements OnInit {
   }
 
   populateSideNavElements(): void {
-    this.addItem({
-      id: 0,
-      name: 'User',
-      icon: 'person',
-      sideNavSectionElement: [
-        {name: 'Sign up', icon: 'person-add', url: '/user/add'},
-        {name: 'Log in', icon: 'log-in', url: '/user/login'},
-        {name: 'Log out', icon: 'log-out', url: '/user/logout'},
-      ]
-    })
-    this.addItem({
-      id: 1,
-      name: 'Group',
-      icon: 'people-circle',
-      sideNavSectionElement: [
-        {name: 'Add group', icon: 'people', url: '/group/add'},
-        {name: 'User\'s groups', icon: 'people', url: '/group/user-groups'}
-      ]
-    })
-    this.addItem({
-      id: 2,
-      name: 'Player',
-      icon: 'accessibility',
-      sideNavSectionElement: [
-        {name: 'Add player', icon: 'person-add', url: '/player/add'},
-        {name: 'Group players list', icon: 'person-add', url: '/player/players-list'},
-      ]
-    })
+    if (this.user) {
+      this.addItem({
+        id: 0,
+        name: 'User',
+        icon: 'person',
+        sideNavSectionElement: [
+          {name: 'Sign up', icon: 'person-add', url: '/user/add'},
+          {name: 'Log in', icon: 'log-in', url: '/user/login'},
+          {name: 'Log out', icon: 'log-out', url: '/user/logout'},
+        ]
+      })
+      this.addItem({
+        id: 1,
+        name: 'Group',
+        icon: 'people-circle',
+        sideNavSectionElement: [
+          {name: 'Add group', icon: 'people', url: '/group/add'},
+          {name: 'User\'s groups', icon: 'people', url: '/group/user-groups'}
+        ]
+      })
+    }
+    if (this.selectedGroup) {
+      this.addItem({
+        id: 2,
+        name: 'Player',
+        icon: 'accessibility',
+        sideNavSectionElement: [
+          {name: 'Add player', icon: 'person-add', url: '/player/add'},
+          {name: 'Group players list', icon: 'person-add', url: '/player/players-list'},
+        ]
+      })
+    }
   }
 
   select(): void {
-    console.log(this.router.getCurrentNavigation()?.id);
+    this.store$.pipe(select(selectSelectedGroup)).pipe(takeUntil(this.ngUnsubscribe)).subscribe(value => {
+      if (value) {
+        this.selectedGroup = value;
+        this.populateSideNavElements();
+      }
+    });
     this.store$.pipe(select(selectLoginUser)).pipe(takeUntil(this.ngUnsubscribe)).subscribe(value => {
       if (value) {
         this.user = value;
@@ -88,6 +100,11 @@ export class SideNavComponent implements OnInit {
   }
 
   addItem(sideNavSelection: SideNavSection): void {
+    this.items.forEach(value => {
+      if (value.id === sideNavSelection) {
+        return;
+      }
+    })
     this.items.push(sideNavSelection);
   }
 }
