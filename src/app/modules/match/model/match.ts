f67f2d25162d63a208d1de20a9a135f29f8e42d9
stefanl <stefan.lezaic@plusplusnt.rs>
2023-02-21 13:00:42 +0100

    [develop] add match

--- src/app/modules/match/model/match.ts
+++ src/app/modules/match/model/match.ts
@@ -1,7 +1,25 @@
-export interface Match {
+import {Team} from "../../team/model/team";
 
+export enum MatchStatus {
+  CREATED = 'CREATED',
+  IN_PROGRESS = 'IN_PROGRESS',
+  END = 'END',
+}
+
+export interface Match {
+  name: string;
+  matchStatus: MatchStatus;
+  firstTeam: Team;
+  secondTeam: Team;
+  groupUuid: string;
+  createdBy: string;
 }
 
 export interface MatchAddRequest {
-
+  name: string;
+  matchStatus: MatchStatus;
+  firstTeamUuid: string;
+  secondTeamUuid: string;
+  groupUuid: string;
+  createdBy: string;
 }
